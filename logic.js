4d019ab0d13cdec166045beefc4981dfec431a55
treblig-punisher <gilbertplayx@hotmail.com>
2021-12-17 02:20:46 -0500

    There was some text missing

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 const getLogo = document.querySelector(".logo");
+const getSection = document.querySelector("section");
+getLogo.addEventListener("animationend", ()=>{
 
-function goToPageToBuy(){
-    
-}
+});
