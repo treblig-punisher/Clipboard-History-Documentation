20e1d4d277ca6a211b0d3dee7d94787612e9d72e
treblig-punisher <gilbertplayx@hotmail.com>
2021-12-17 02:39:14 -0500

    animation timing added
    needed to start the element off screen with margin-left: the value needed and using animation-delay.

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
 const getLogo = document.querySelector(".logo");
-const getSection = document.querySelector("section");
-getLogo.addEventListener("animationend", ()=>{
-
-});
+const getSection = document.querySelector(".main-section-wrapper");
+// getLogo.addEventListener("animationend", ()=>{
+//      getSection.WebkitAnimationName = "section-animation";
+//     // getSection.webkitAnimationFillMode ="forwards";
+//     // getSection.WebkitAnimationPlayState = "running";
+// });
