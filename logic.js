06e91490217a934ed6d5dddb29a7b216cf01a02d
treblig-punisher <gilbertplayx@hotmail.com>
2022-01-12 14:26:09 -0500

    Logo  and shortcuts have been updated

--- logic.js
+++ logic.js
@@ -5,3 +5,17 @@ const getSection = document.querySelector(".main-section-wrapper");
 //     // getSection.webkitAnimationFillMode ="forwards";
 //     // getSection.WebkitAnimationPlayState = "running";
 // });
+function checkElemWidth(elem) {
+    if (elem.clientWidth > window.innerWidth) {
+      console.info(
+        "The following element has a larger width than " +
+        "the window’s outer width"
+      );
+      console.info(elem);
+      console.info("\n\n");
+    }
+    // Recursively check all the children
+    // of the element to find the culprit.
+    [...elem.children].forEach(checkElemWidth);
+  }
+  checkElemWidth(document.body);
